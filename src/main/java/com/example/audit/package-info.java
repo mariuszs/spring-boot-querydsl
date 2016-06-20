ac4ab26a2754018f8d48a6bfcc6ac4b1f149874d
Mariusz Smykula <mariuszs@gmail.com>
2016-06-20 19:12:48 +0200

    downgrade do spring-data-jpa supported versions

--- src/main/java/com/example/audit/package-info.java
+++ src/main/java/com/example/audit/package-info.java
@@ -1,5 +1,5 @@
 @QueryEntities({DefaultRevisionEntity.class})
 package com.example.audit;
 
-import com.querydsl.core.annotations.QueryEntities;
+import com.mysema.query.annotations.QueryEntities;
 import org.hibernate.envers.DefaultRevisionEntity;
